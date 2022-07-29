63f7ad9807d7421032b7570c668c8a66caeb6a8f
Sultepronit <sultepronit@gmail.com>
2022-07-29 22:40:40 +0300

    learned about casting numerical values

---
+++ Casting/src/App.java
@@ -0,0 +1,24 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		byte byteValue = 20;
+		short shortvalue = 99;
+		int intValue = 654;
+		long longVlue = 654646;
+		
+		float floatValue = 65464.6546f;
+		double doubleValue = 46464.64554;
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		byteValue = (byte) 128;
+		System.out.println(byteValue);
+		
+		byteValue = (byte) 138;
+		System.out.println(byteValue);
+	}
+	
+
+}
