74622899879734a1b1c82cc6c340de353647d350
Sultepronit <sultepronit@gmail.com>
2022-07-31 17:10:02 +0300

    learned about reading text files

--- Reading text files/src/App.java
+++ Reading text files/src/App.java
@@ -1,8 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		//String fileName = "/home/step/Main/java1/Text.txt";
+		String fileName = "Text.txt";
+		
+		File textFile = new File(fileName);
+		
+		Scanner in = new Scanner(textFile);
+		
+		int number = in.nextInt();
+		in.nextLine(); // read the invisible symbols
+		
+		System.out.println("First line number: " + number);
+		
+		while(in.hasNextLine()) {
+			String line = in.nextLine();
+			
+			System.out.println(line);
+		}
+		
+		in.close();
 
 	}
 
