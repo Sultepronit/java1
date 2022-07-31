5f19ddf253392e8163fa6026ae9929da7b4b1052
Sultepronit <sultepronit@gmail.com>
2022-07-31 13:15:14 +0300

    learned about wildcards

--- Generics and wildcards/src/App.java
+++ Generics and wildcards/src/App.java
@@ -6,6 +6,10 @@ class Machine {
 	public String toString() {
 		return "I am a machine";
 	}
+	
+	public void start() {
+		System.out.println("Machine started.");
+	}
 }
 
 class Camera extends Machine {
@@ -13,6 +17,10 @@ class Camera extends Machine {
 	@Override
 	public String toString() {
 		return "I am a camera";
+	}
+	
+	public void snap() {
+		System.out.println("Snap!");
 	}
 }
 
@@ -32,6 +40,30 @@ public class App {
 		list2.add(new Machine());
 		
 		showList2(list2);
+		
+		ArrayList<Camera> list3 = new ArrayList<>();
+		list3.add(new Camera());
+		list3.add(new Camera());
+		
+		// not working
+		//showList2(list3);
+		
+		showList3(list3);
+		
+		showList4(list2);
+		
+		showList4(list3);
+		
+		showList5(list3);
+		
+		showList3(list);
+		
+		//showList6(list); // editor shows error
+		// works only with Camera & its superclass Machine
+		showList6(list2);
+		showList6(list3);
+		
+		//showList5(list); // runtime error
 
 	}
 	
@@ -44,6 +76,45 @@ public class App {
 	public static void showList2(ArrayList<Machine> list) {
 		for(Machine value : list) {
 			System.out.println(value);
+		}
+	}
+	
+	public static void showList3(ArrayList<?> list) {
+		for(Object value : list) {
+			System.out.println(value);
+		}
+	}
+	
+	public static void showList4(ArrayList<? extends Machine> list) {
+		for(Machine value : list) {
+			System.out.println(value);
+			value.start();
+			// can't work
+			//value.snap();
+		}
+	}
+	
+	public static void showList5(ArrayList<?> list) {
+		for(Object value : list) {
+			System.out.println(value);
+			
+			Machine mach = (Machine) value;
+			mach.start();
+			
+			Camera cam = (Camera) value;
+			cam.snap();
+		}
+	}
+	
+	public static void showList6(ArrayList<? super Camera> list) {
+		for(Object value : list) {
+			System.out.println(value);
+			
+			/*Machine mach = (Machine) value;
+			mach.start();
+			
+			Camera cam = (Camera) value;
+			cam.snap();*/
 		}
 	}
 
