f758f5d692286daee00649e4d0b6bca382340772
Sultepronit <sultepronit@gmail.com>
2022-08-04 14:57:24 +0300

    Initial commit of Tower of Hannoi (absolutely don't know what to do)

---
+++ Tower of Hanoi/src/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// I know that I must do it, and I know that it should be done with a recursive method
+		// But I still don't understand what to do...
+
+	}
+
+}
